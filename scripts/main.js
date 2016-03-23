ff6fdcc7372b775ccfbd7543fdf0ee24a048b868
anna hunter <annfmonty@gmail.com>
2016-03-22 22:37:02 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+
+
+
+var etsydata = $.ajax({
+	url: "https://api.etsy.com/v2/listings/active.js?api_key=cs08kp5uc0o3f5hgkek1lp4d&keywords=ninja&includes=Images,Shop`",
+	type: "GET",
+	dataType: 'jsonp',
+	success: function(data) {
+		console.log(data);
+		// for(var i = 0; i < 25; i++){
+		// 	[etsydata].indexOf("index", "title", "price");
+		// 	console.log(datalist);
+		data.results.forEach(function(listing){
+			var title = listing.title;
+			var image = listing.Images[0].url_170x135;
+			var desc = listing.description;
+			var price = listing.price;
+
+			$( ".displayDiv" ).append
+  			 ("<div>" + "<img src ='"+image+"'/>" 
+  			 	+ "<span>" + title  + "</span>" + "<span>" 
+  			 	+price+ "</span>" + "</div>");
+
+  			//console.log(listing.title);
+		})
+	}
+});
+
+
+
+
+
+
+ 
+
+
